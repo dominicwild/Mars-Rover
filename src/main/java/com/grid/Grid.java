429d8100a2e727b334395784e461c6ac7c700fa8
dwild8 <dwild8@dxc.com>
2022-01-01 19:28:53 +0000

    Grid with basic obstacle placement

---
+++ src/main/java/com/grid/Grid.java
@@ -0,0 +1,34 @@
+package com.grid;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Grid {
+
+	private static final int GRID_HEIGHT = 10;
+	private static final int GRID_WIDTH = 10;
+
+	Map<String, Boolean> obstacles = new HashMap<>();
+
+	public int getWidth() {
+		return GRID_WIDTH;
+	}
+
+	public int getHeight() {
+		return GRID_HEIGHT;
+	}
+
+	public void placeObstacle(int i, int j) {
+		boolean obstaclePlaced = true;
+		obstacles.put(coordinateKey(i, j), obstaclePlaced);
+	}
+
+	private String coordinateKey(int i, int j) {
+		return i + "," + j;
+	}
+
+	public boolean isObstacle(int i, int j) {
+		return obstacles.getOrDefault(coordinateKey(i, j), false);
+	}
+
+}
---
+++ src/test/java/com/grid/GridTest.java
@@ -0,0 +1,35 @@
+package com.grid;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class GridTest {
+
+	@Test
+	void grid_is_created_with_default_size_10_10() {
+		Grid grid = new Grid();
+		assertEquals(10, grid.getWidth());
+		assertEquals(10, grid.getHeight());
+	}
+
+	@Test
+	void obstacle_can_be_placed_in_the_grid() {
+		Grid grid = new Grid();
+		grid.placeObstacle(1, 1);
+		assertTrue(grid.isObstacle(1, 1));
+	}
+
+	@Test
+	void space_in_grid_has_no_obstacles_when_created(){
+		Grid grid = new Grid();
+		for (int i = 0; i < grid.getWidth(); i++) {
+			for (int j = 0; j < grid.getHeight(); j++) {
+				assertFalse(grid.isObstacle(i, j));
+			}
+		}
+	}
+
+}
